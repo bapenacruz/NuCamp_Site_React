3e7f9e8b225cc33fdedbfca54da58501bdf829e9
Bryan <bapenacruz@gmail.com>
2020-09-27 18:16:27 -0400

    09/27/2020

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -25,8 +25,8 @@ class CommentForm extends React.Component {
     }
 
     handleSubmit(values) {
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -116,13 +116,13 @@ function RenderCampsite({campsite}){
         );
     }
 
-function RenderComments({comments}){
+function RenderComments({comments, addComment, campsiteId}){
         if (comments){
             return(
             <div className="col-md-5 m-1">
                 <h4>COMMENTS:</h4>
                 {comments.map(comments => <p key={comments.id}>{comments.text}<br/> --- {comments.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</p>)}
-                <CommentForm/>
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
             );
         } else {
@@ -148,7 +148,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
             );
---
+++ src/redux/campsites.js
@@ -0,0 +1,8 @@
+import { CAMPSITES } from '../shared/campsites';
+
+export const Campsites = (state = CAMPSITES, actions) => {
+    switch (actions.type){
+        default:
+            return state;
+    }
+};
